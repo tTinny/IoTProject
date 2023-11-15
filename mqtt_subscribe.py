3b40fca58f7b5da878825321bdea9cef178d17b0
Prakriti <prakritiarora.13@gmail.com>
2023-11-15 00:58:05 +0000

    Add files via upload

---
+++ mqtt_subscribe.py
@@ -0,0 +1,30 @@
+import json
+from paho.mqtt import client as mqtt_client
+
+if __name__ == '__main__':
+    clientID = 'test'
+    mqtt_ip = "localhost"
+    mqtt_port = 1883
+    topic = "python/mqtt"
+
+    # Callback function for MQTT connection
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT OK!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    # Connect to MQTT service
+    client.on_connect = on_connect
+    client.connect(mqtt_ip, mqtt_port)
+
+    # Callback function will be triggered
+    def on_message(client, userdata, msg):
+        print(f"Get message from publisher {json.loads(msg.payload)}")
+
+    # Subscribe MQTT topic
+    client.subscribe(topic)
+    client.on_message = on_message
+
+    # Start a thread to monitor message from publisher
+    client.loop_forever()

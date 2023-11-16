f4e77436d5ad131a258f20b83b07c5be435d8cf4
Prakriti <prakritiarora.13@gmail.com>
2023-11-16 04:25:48 +0000

    Add files via upload

--- rabbitmq_consume.py
+++ rabbitmq_consume.py
@@ -1,6 +1,8 @@
 import json
 import pika
 from datetime import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def fetch_data():
 
@@ -23,11 +25,37 @@ def fetch_data():
     # Declare a queue
     channel.queue_declare(queue=rabbitmq_queque)
 
-    channel.basic_consume(queue=rabbitmq_queque,
+    message = channel.basic_consume(queue=rabbitmq_queque,
                           auto_ack=True,
                           on_message_callback=callback)
+
+    print(message)
+    
+    # Prepare data
+    data = {
+        'Timestamp': ['01/09','02/09','03/09','04/09','05/09'],
+        'Value': [1,2,1,10,5]
+    }
+
+    data_df = pd.DataFrame(data)
+
+    # Initialize a canvas
+    plt.figure(figsize=(8, 4), dpi=200)
+    # Plot data into canvas
+    plt.plot(data_df["Timestamp"], data_df["Value"], color="#FF3B1D", marker='.', linestyle="-")
+    plt.title("Example data for demonstration")
+    plt.xlabel("DateTime")
+    plt.ylabel("Value")
+
+    # Save as file
+    plt.savefig("figure1.png")
+    # Directly display
+    plt.show()
 
     channel.start_consuming()
 
 if __name__ == '__main__':
     fetch_data()
+
+
+    

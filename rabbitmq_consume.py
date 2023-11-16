8fb805a78792f3a45755bceb750019ddd54d9419
Prakriti <prakritiarora.13@gmail.com>
2023-11-16 10:17:41 +0000

    Add files via upload

--- rabbitmq_consume.py
+++ rabbitmq_consume.py
@@ -14,9 +14,11 @@ def fetch_data():
     def callback(ch, method, properties, body):
         msg = json.loads(body)
         timestamp = msg[10:20]
-        date = datetime.fromtimestamp(timestamp)
-        value = f"{msg[:10]}{date}{msg[23:]}"
-        print(f"Got message from producer msg: {value}")
+        if timestamp != 'None':
+            date = datetime.fromtimestamp(timestamp)
+            value = f"{msg[:10]}{date}{msg[23:]}"
+            print(f"Got message from producer msg: {value}")
+
 
     # Connect to RabbitMQ service with timeout 1min
     connection = pika.BlockingConnection(
@@ -25,16 +27,19 @@ def fetch_data():
     # Declare a queue
     channel.queue_declare(queue=rabbitmq_queque)
 
-    message = channel.basic_consume(queue=rabbitmq_queque,
+    channel.basic_consume(queue=rabbitmq_queque,
                           auto_ack=True,
                           on_message_callback=callback)
 
-    print(message)
-    
+    channel.start_consuming()
+
+def plotdata():
     # Prepare data
+    timestamp = []
+    value = []
     data = {
-        'Timestamp': ['01/09','02/09','03/09','04/09','05/09'],
-        'Value': [1,2,1,10,5]
+        'Timestamp': ['01/11','02/11','03/11','04/11','05/11','06/11','07/11','08/11','09/11','10/11','11/11','12/11','13/11','14/11','15/11','16/11','17/11'],
+        'Value': [2.122,5.226,0.65,2.498,5.297,8.882,2.886,3.209,5.453,8.537,3.796,12.58,13.52,1.142,8.551,11.92]
     }
 
     data_df = pd.DataFrame(data)
@@ -52,10 +57,9 @@ def fetch_data():
     # Directly display
     plt.show()
 
-    channel.start_consuming()
-
 if __name__ == '__main__':
     fetch_data()
+    plotdata()
 
 
     
